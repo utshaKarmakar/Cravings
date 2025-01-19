ace32ddc482b18c351a03f5a9f38503d114fa301
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-01-20 02:00:04 +0600

    user table updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->unique();
+            $table->string('address')->unique();
+            $table->string('role')->default('user');
+            $table->string('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
