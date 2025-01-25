ede6f106cbfe733e23d9d7054d164405d489b28a
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-01-26 05:41:29 +0600

    admin dashboard updated

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Minia.
+                <script>document.write(new Date().getFullYear())</script> 
+                © Cravings
             </div>
 
         </div>
