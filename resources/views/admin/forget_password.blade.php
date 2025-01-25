c58dda57d58685f0e0553b261ed7358c15ea369b
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-01-26 00:22:08 +0600

    Admin reset password done

--- resources/views/admin/forget_password.blade.php
+++ resources/views/admin/forget_password.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login Page</title>
+    <title>Forget Password Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="container">
-    <h1>Admin Login</h1>
+    <h1>Forget Password</h1>
 
     @if ($errors -> any())
         @foreach ($errors->all() as $error)
@@ -23,22 +23,15 @@
         <li>{{ Session::get('success')}}</li>
     @endif   
 
-    <form action="{{route('admin.login_submit')}}" method="post">
+    <form action="{{route('admin.password_submit')}}" method="post">
         @csrf
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <a href="{{ route('admin.forget_password') }}">Forget Password</a>
-        </div>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">EMAIL PASSWORD RESET LINK</button>
     </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,11 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.
 Route::post('/admin/login_submit', [AdminController::class, 'AdminLoginSubmit'])->name('admin.login_submit');
 
 Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+Route::get('/admin/forget_password', [AdminController::class, 'AdminForgetPassword'])->name('admin.forget_password');
+
+Route::post('/admin/password_submit', [AdminController::class, 'AdminPasswordSubmit'])->name('admin.password_submit');
+
+Route::get('/admin/reset_password/{token}/{email}', [AdminController::class, 'AdminResetPassword']);
+
+Route::post('/admin/reset_password_submit', [AdminController::class, 'AdminResetPasswordSubmit'])->name('admin.reset_password_submit');
